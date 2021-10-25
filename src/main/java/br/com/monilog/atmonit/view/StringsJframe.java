67d10352868992b6071a1ad45ff57722b4da2e52
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-10-24 21:27:59 -0300

    adiociona arquivo somente para strings de utilização do jframe

--- src/main/java/br/com/monilog/atmonit/view/StringsJframe.java
+++ src/main/java/br/com/monilog/atmonit/view/StringsJframe.java
@@ -1,2 +1,16 @@
-package br.com.monilog.atmonit.view;public class StringsJframe {
+package br.com.monilog.atmonit.view;
+
+import javax.swing.*;
+
+public class StringsJframe {
+
+    public String image = "C:\\Users\\Dell\\Downloads\\Repositories\\atmonit-monitoring-application\\src\\main\\java\\br\\com\\monilog\\atmonit\\view\\systemtray\\icon.png";
+    public String loginSucess = "Login realizado com sucesso!";
+    public String identifySucess = "Identificacao do cadastro da maquina feito com sucesso, Iniciando o monitoramento dos recursos... ";
+    public String addressSave = "Endereço salvo id: ";
+    public String terminalSave = "Terminal salvo id: ";
+    public String terminalRegistered = "Terminal cadastrado com sucesso, iniciando monitoramento de recursos!";
+    public String loginIncorrect = "Login incorreto, tente novamente";
+    public String impossibleIdentify = "Nao foi possivel identificarmos esse terminal\n" + "Por favor, insira o cep onde o terminal esta situado";
+
 }
