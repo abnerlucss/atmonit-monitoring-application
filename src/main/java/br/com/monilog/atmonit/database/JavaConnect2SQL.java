f8ac49af56312a05d6864abb85beb936bb3543a6
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-10-24 21:30:31 -0300

    adiciona arquivo com configurações sql server

--- src/main/java/br/com/monilog/atmonit/database/JavaConnect2SQL.java
+++ src/main/java/br/com/monilog/atmonit/database/JavaConnect2SQL.java
@@ -1,2 +1,28 @@
-package br.com.monilog.atmonit.database;public class JavaConnect2SQL {
+package br.com.monilog.atmonit.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JavaConnect2SQL {
+
+    public void connectionSQLServer() {
+        String url = "jdbc:sqlserver://srvalunoatmonit.database.windows.net:1433;database=bdProjeto2sem;user=atmonit@srvalunoatmonit;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        String user = "atmonit";
+        String password = "Bandtec123";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conectado ao Microsoft Azure!");
+            String sql =  "insert into teste (nome, senha) values ('brurola','789');";
+            Statement statement = connection.createStatement();
+            Integer rows = statement.executeUpdate(sql);
+            connection.close();
+
+        } catch (SQLException throwables) {
+            System.out.println("Opps, temos um erro:");
+            throwables.printStackTrace();
+        }
+    }
 }
