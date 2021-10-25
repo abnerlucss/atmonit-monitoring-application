f922ed0855cc4b96a743029416e9cfc81be01c03
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-10-24 21:24:52 -0300

    adiciona arquivo para trocar ambiente dev e prod

--- src/main/java/br/com/monilog/atmonit/database/SwitchConnection.java
+++ src/main/java/br/com/monilog/atmonit/database/SwitchConnection.java
@@ -1,2 +1,12 @@
-package br.com.monilog.atmonit.database;public class SwitchConnection {
+package br.com.monilog.atmonit.database;
+
+public class SwitchConnection {
+    // DEV Environment and PROD Production
+    private String environment = "DEV";
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+
 }
