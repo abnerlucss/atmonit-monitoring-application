1068d2aa1102a0c7f81d073562968e4cb162b255
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-06 00:54:27 -0300

    altera connection azure

--- src/main/java/br/com/monilog/atmonit/database/JavaConnect2SQL.java
+++ src/main/java/br/com/monilog/atmonit/database/JavaConnect2SQL.java
@@ -1,28 +1,20 @@
 package br.com.monilog.atmonit.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.*;
 
 public class JavaConnect2SQL {
-    public static void main(String[] args) {
+    private BasicDataSource dataSource;
 
-        String url = "jdbc:sqlserver://srvalunoatmonit.database.windows.net:1433;database=bdProjeto2sem;user=atmonit@srvalunoatmonit;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-        String user = "atmonit";
-        String password = "Bandtec123";
+    public JavaConnect2SQL() {
+        dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:sqlserver://srvalunoatmonit.database.windows.net:1433;database=bdProjeto2sem;user=atmonit@srvalunoatmonit;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+        dataSource.setUsername("atmonit");
+        dataSource.setPassword("Bandtec123");
+    }
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Conectado ao Microsoft Azure!");
-            String sql = "insert into teste (nome, senha) values ('abnim','789');";
-            Statement statement = connection.createStatement();
-            Integer rows = statement.executeUpdate(sql);
-            connection.close();
-
-        } catch (SQLException throwables) {
-            System.out.println("Opps, temos um erro:");
-            throwables.printStackTrace();
-        }
+    public BasicDataSource getDataSource() {
+        return dataSource;
     }
 }
