3f4088cbf91e1f1ab8efbdafafafe413a1066c05
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-06 11:05:29 -0300

    FEAT(EmployeeDAO.java):adiciona metodo de connexao com bd azure

--- src/main/java/br/com/monilog/atmonit/dao/EmployeeDAO.java
+++ src/main/java/br/com/monilog/atmonit/dao/EmployeeDAO.java
@@ -1,12 +1,36 @@
 package br.com.monilog.atmonit.dao;
 
 import br.com.monilog.atmonit.database.ConnectionFactory;
+import br.com.monilog.atmonit.database.JavaConnect2SQL;
+import br.com.monilog.atmonit.database.SwitchConnection;
 import br.com.monilog.atmonit.model.EmployeeLogin;
 import org.springframework.jdbc.core.JdbcTemplate;
+import java.sql.*;
 
-public class EmployeeDAO implements IFuncionarioDAO {
+public class EmployeeDAO extends JavaConnect2SQL implements IFuncionarioDAO {
+
+    public Integer loginFuncionarioAzure(EmployeeLogin employeeLogin) {
+        JavaConnect2SQL javaConnect2SQL = new JavaConnect2SQL();
+        Integer idCompany = null;
+
+        try {
+            Connection connection = DriverManager.getConnection(getDataSource().getUrl(), getDataSource().getUsername(), getDataSource().getPassword());
+
+            Statement statement = connection.createStatement();
+            ResultSet rows = statement.executeQuery(String.format("select c.id_company from employee as e join company as c on e.fk_company = c.id_company" +
+                            " where c.company_name = \'%s\' and e.login = \'%s\' and e.password = \'%s\'", employeeLogin.getCompany(),
+                    employeeLogin.getLogin(), employeeLogin.getPassword()));
+            connection.close();
+        } catch (SQLException throwables) {
+            System.out.println("Opps, temos um erro:");
+            throwables.printStackTrace();
+        }
+        return idCompany;
+
+    }
+
     @Override
-    public Integer loginFuncionario(EmployeeLogin employeeLogin) {
+    public Integer loginFuncionarioSQL(EmployeeLogin employeeLogin) {
         ConnectionFactory config = new ConnectionFactory();
         JdbcTemplate con = new JdbcTemplate(config.getDataSource());
         Integer idCompany = null;
