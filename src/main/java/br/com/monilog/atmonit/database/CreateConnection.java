60384f3ee04a5879bd1d523797b019fb4d9c88f5
KennedyMartinsB <Kennedy.Martins@bandtec>
2021-11-20 11:03:21 -0300

    Revert "Primeira versão do log"
    This reverts commit bd8afaf299e5cf16c63a629b853ac6ed4c1496b4.

--- src/main/java/br/com/monilog/atmonit/database/CreateConnection.java
+++ src/main/java/br/com/monilog/atmonit/database/CreateConnection.java
@@ -1,20 +1,14 @@
 package br.com.monilog.atmonit.database;
 
-import br.com.monilog.atmonit.view.Log;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class CreateConnection {
 
     public Connection createConnection() throws SQLException {
-        
-        Connection connection = null;
-        
-        try{
-             SwitchConnection switchConnection = new SwitchConnection();
+        SwitchConnection switchConnection = new SwitchConnection();
 
-            connection = null;
+        Connection connection = null;
 
         if (switchConnection.getEnvironment().equals("DEV")) {
             ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -23,10 +17,6 @@ public class CreateConnection {
             JavaConnect2SQL javaConnect2SQL = new JavaConnect2SQL();
             connection = javaConnect2SQL.recoverConnectionAzure();
         }
-        Log.logWriter("> Conexão realizada <");
-        }catch(Exception ex){
-        //Configurar classe de excessão;
-    }
         return connection;
     }
 }
