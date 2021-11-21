dd774cb619d1b5df4952a2eae7f0bd3be4f999f5
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-21 19:04:18 -0300

    docker

--- src/main/java/br/com/monilog/atmonit/database/ConnectionFactory.java
+++ src/main/java/br/com/monilog/atmonit/database/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 
     public Connection recoverConnectionSQL() throws SQLException {
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/atmonit?useTimezone=true&serverTimezone=UTC",
+                "jdbc:mysql://3.95.157.176:3306/atmonit?useTimezone=true&serverTimezone=UTC",
                 "atmonit",
                 "bandtec");
     }
