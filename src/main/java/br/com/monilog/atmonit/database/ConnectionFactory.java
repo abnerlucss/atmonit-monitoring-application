e22561e15a88947c634354711279582a6dbbfccf
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-21 21:16:37 -0300

    jar

--- src/main/java/br/com/monilog/atmonit/database/ConnectionFactory.java
+++ src/main/java/br/com/monilog/atmonit/database/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 
     public Connection recoverConnectionSQL() throws SQLException {
         return DriverManager.getConnection(
-                "jdbc:mysql://34.238.82.239:3306/atmonit?useTimezone=true&serverTimezone=UTC",
+                "jdbc:mysql://3.86.42.78:3306/atmonit?useTimezone=true&serverTimezone=UTC",
                 "atmonit",
                 "bandtec");
     }
