0189e856c9e08e35ea86f7812de203fc5d0eb36d
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-28 18:02:45 -0300

    chave webhook

--- src/main/java/br/com/monilog/atmonit/webhook/Slack.java
+++ src/main/java/br/com/monilog/atmonit/webhook/Slack.java
@@ -7,10 +7,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Base64;
 
 public class Slack {
+
+    private static final byte[] decodedBytes = Base64.getUrlDecoder().decode("aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDAyUEgzNUJYNFkvQjAyTlRKVjhNTDIvYWltMGlUT1RRODhpYVcxVE1mZ2xESFZT");
+    private static final String decodedUrl = new String(decodedBytes);
+
     private static HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String URL = decodedUrl;
 
     public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
 
