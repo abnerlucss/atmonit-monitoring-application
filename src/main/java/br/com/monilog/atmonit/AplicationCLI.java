6588aebfcf64cde3d7bfe6ea5b43612228803840
CoelhoRafael <rafael.coelho@bandtec.com.br>
2021-11-28 15:48:12 -0300

    adiciona tratativa de erros

--- src/main/java/br/com/monilog/atmonit/AplicationCLI.java
+++ src/main/java/br/com/monilog/atmonit/AplicationCLI.java
@@ -3,12 +3,13 @@ package br.com.monilog.atmonit;
 import Log.Log;
 import br.com.monilog.atmonit.view.LoginCLI;
 
+import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
 
 public class AplicationCLI {
-    public static void main(String[] args) throws SocketException, UnknownHostException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, InterruptedException {
         Log log = new Log();
 
         log.saveLog("INFO: Iniciando a aplicacao via CLI.");
